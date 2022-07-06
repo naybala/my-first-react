5976b76f1972c77903d52249bbef5413642a288c
naybala <naybalaismylife.nbl@gmail.com>
2022-07-06 13:39:17 +0630

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from"react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="container mt-5">
+      <h1 className="text-primary">Hello React</h1>
+      <br/>
+    <button className="btn btn-sm btn-warning">Testing React </button>
     </div>
   );
 }
-
 export default App;
